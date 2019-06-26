cee80abd36d8a5e4cddfc431cd88f7c1796dabc4
Andrew Hok <ahok89@gmail.com>
2019-06-26 19:14:34 -0400

    added function

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+concert-this = function () {
+    https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+}
+
+spotify-this-song
+
+movie-this
+
+do-what-it-says
